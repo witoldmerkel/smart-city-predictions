dac945d8a764d5290cf23d8d9d95ea2a2a289665
Witold <w.merkel@student.mini.pw.edu.pl>
2020-10-14 19:06:54 +0200

    kafka-faza-druga

--- Kafka/producer.py
+++ Kafka/producer.py
@@ -1,12 +1,21 @@
 from time import sleep
 from json import dumps
 from kafka import KafkaProducer
+import requests
 
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
                          value_serializer=lambda x:
                          dumps(x).encode('utf-8'))
 
-for e in range(1000):
-    data = {'number' : e}
-    producer.send('numtest', value=data)
+
+
+while(1):
+    response1 = requests.get("https://api.waqi.info/feed/warsaw/?token=5daa289b09c26a80ee8dfb9f41a19fafd067c10b")
+    data1 = response1.json()
+    producer.send('numtest', value=data1)
+
+    response2 = requests.get("https://api.waqi.info/feed/paris/?token=5daa289b09c26a80ee8dfb9f41a19fafd067c10b")
+    data2 = response2.json()
+    producer.send('numtest', value=data2)
+
     sleep(5)
