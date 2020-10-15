1f85fef0e8725ff9bf1521b2473ccac31e093b26
michalSt98 <32554595+michalSt98@users.noreply.github.com>
2020-10-15 15:23:55 +0200

    Spark config

--- Kafka/spark_configuration.py
+++ Kafka/spark_configuration.py
@@ -1,5 +1,5 @@
 import findspark
-findspark.init()
+findspark.init("C:\Tools\spark\spark-3.0.1-bin-hadoop2.7")
 
 from pyspark import SparkContext
 from pyspark import SparkConf
--- Kafka/spark_reader.py
+++ Kafka/spark_reader.py
@@ -6,14 +6,13 @@ from pyspark.streaming.kafka import KafkaUtils
 from pyspark.sql.context import SQLContext
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print("Usage: kafka_wordcount.py <zk> <topic>", file=sys.stderr)
-        exit(-1)
+
 
     sc = SparkContext(appName="PythonStreamingKafkaWordCount")
     ssc = StreamingContext(sc, 1)
 
-    zkQuorum, topic = sys.argv[1:]
+    zkQuorum = 1
+    topic = "numtest"
     kvs = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-consumer", {topic: 1})
     lines = kvs.map(lambda x: x[1])
     lines.pprint()
