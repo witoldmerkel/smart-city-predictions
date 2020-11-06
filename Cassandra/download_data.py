868db17c8e62e5119c8c79c65bffe60e32d06f8f
Witold <w.merkel@student.mini.pw.edu.pl>
2020-11-06 16:50:09 +0100

    working data download

--- Cassandra/download_data.py
+++ Cassandra/download_data.py
@@ -1,27 +1,8 @@
-import pandas as pd
-
 from cassandra.cluster import Cluster
-from cassandra.auth import PlainTextAuthProvider
-from cassandra.query import dict_factory
-
-
-cluster = Cluster(contact_points=["127.0.0.1"], port="9042")
-session = cluster.connect("json")
-
-
-def pandas_factory(colnames, rows):
-    return pd.DataFrame(rows, columns=colnames)
-
-
-session.row_factory = pandas_factory
-session.default_fetch_size = None
-
-
-query = "SELECT * FROM {}.{};".format("json", "velib")
-
-df = pd.DataFrame()
-
-rslt = session.execute(query, timeout=None)
-df = rslt._current_rows
-
-print(df.shape)
+cluster = Cluster(['127.0.0.1'], "9042")  # provide contact points and port
+session = cluster.connect('json')
+urzedy = session.execute('select * from urzedy;')
+powietrze = session.execute('select * from powietrze;')
+velib = session.execute('select * from velib;')
+velib_stacje = session.execute('select * from velib_stations;')
+urzedy_nazwy = session.execute('select * from urzedy_nazwy;')
