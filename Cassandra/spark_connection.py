7861c79d098cb4c85371c67d3f9840a8a114487b
michalSt98 <32554595+michalSt98@users.noreply.github.com>
2020-11-07 21:39:55 +0100

    Working spark 1 city

---
+++ Cassandra/spark_connection.py
@@ -0,0 +1,39 @@
+import os
+from pyspark import SparkContext
+from pyspark.sql import SQLContext
+
+
+os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages com.datastax.spark:spark-cassandra-connector_2.12:3.0.0 --conf spark.cassandra.connection.host=127.0.0.1 pyspark-shell'
+
+sc = SparkContext("local", "json")
+sqlContext = SQLContext(sc)
+
+
+def load_and_get_table_df(keys_space_name, table_name):
+    table_df = sqlContext.read\
+        .format("org.apache.spark.sql.cassandra")\
+        .options(table=table_name, keyspace=keys_space_name)\
+        .load()
+    return table_df
+
+
+velib = load_and_get_table_df("json", "velib")
+
+
+velib.show()
+velib.groupBy("station_id").count().orderBy('count', ascending=False).show()
+
+velib.printSchema()
+
+firstStation = velib.where(velib["station_id"] == 102328355).select("ebike")
+
+# Teraz dopiero zachodzi pobranie
+
+firstStation = firstStation.cache()
+
+# Usuwanie danych z pamięci
+
+firstStation.unpersist()
+
+# Shutdowning PySpark Context
+sc.stop()
