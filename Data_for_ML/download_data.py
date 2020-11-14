35f55ef14bd7c81d0f14b1146ea8c1ddc353a186
Witold <w.merkel@student.mini.pw.edu.pl>
2020-11-14 20:57:37 +0100

    interfejs, gotowe archiwalne dane dla powietrza i  rowerów

--- Data_for_ML/download_data.py
+++ Data_for_ML/download_data.py
@@ -2,14 +2,14 @@ from cassandra.cluster import Cluster
 import pandas as pd
 from datetime import datetime
 import time
-
+start_time = time.time()
 cluster = Cluster(['127.0.0.1'], "9042")
 session = cluster.connect('json')
-miasto = "Paris"
-cql = "select * from powietrze where name = " + str(miasto)
-powietrze = session.execute(cql)
+cql = "select * from velib where station_id = 102328355"
+velib = session.execute(cql)
 df = pd.DataFrame()
-for row in powietrze:
-    print(pd.DataFrame(row))
+for row in velib:
+    df = df.append(pd.DataFrame(row))
 
+print("--- %s seconds ---" % (time.time() - start_time))
 
--- Flask/server.py
+++ Flask/server.py
@@ -13,11 +13,37 @@ def home():
 def powietrze():
     return render_template("powietrze.html")
 
+@app.route("/velib")
+def velib():
+    return render_template("velib.html")
+
 @app.route("/powietrze/dane/<miasto>/<fromd>/<tod>", methods=['GET'])
 def get_powietrze(miasto, fromd, tod):
     cluster = Cluster(['127.0.0.1'], "9042")
     session = cluster.connect('json')
     cql = "SELECT json * FROM powietrze where name =" + miasto + " and timestamp >" + fromd + " and timestamp <" + tod
+    r = session.execute(cql)
+    df = pd.DataFrame()
+    for row in r:
+        df = df.append(pd.DataFrame(row))
+    return str(df.to_json(orient="records"))
+
+@app.route("/velib/stacje")
+def get_stacje():
+    cluster = Cluster(['127.0.0.1'], "9042")
+    session = cluster.connect('json')
+    cql = "SELECT json * from velib_stations"
+    r = session.execute(cql)
+    df = pd.DataFrame()
+    for row in r:
+        df = df.append(pd.DataFrame(row))
+    return str(df.to_json(orient="records"))
+
+@app.route("/velib/dane/<stacja>/<fromd>/<tod>", methods=['GET'])
+def get_rowery(stacja, fromd, tod):
+    cluster = Cluster(['127.0.0.1'], "9042")
+    session = cluster.connect('json')
+    cql = "SELECT json * FROM velib where station_id =" + stacja + " and timestamp >" + fromd + " and timestamp <" + tod
     r = session.execute(cql)
     df = pd.DataFrame()
     for row in r:
