e80c97aa79e3e9901ec640ad6459641414928a8e
Witold <w.merkel@student.mini.pw.edu.pl>
2020-11-15 20:26:33 +0100

    pokazywanie danych archiwalnych w interfejsie

--- Flask/server.py
+++ Flask/server.py
@@ -17,8 +17,12 @@ def powietrze():
 def velib():
     return render_template("velib.html")
 
+@app.route("/urzedy")
+def urzedy():
+    return render_template("urzedy.html")
+
 @app.route("/powietrze/dane/<miasto>/<fromd>/<tod>", methods=['GET'])
-def get_powietrze(miasto, fromd, tod):
+def get_powietrze_dane_archiwalne(miasto, fromd, tod):
     cluster = Cluster(['127.0.0.1'], "9042")
     session = cluster.connect('json')
     cql = "SELECT json * FROM powietrze where name =" + miasto + " and timestamp >" + fromd + " and timestamp <" + tod
@@ -40,10 +44,32 @@ def get_stacje():
     return str(df.to_json(orient="records"))
 
 @app.route("/velib/dane/<stacja>/<fromd>/<tod>", methods=['GET'])
-def get_rowery(stacja, fromd, tod):
+def get_rowery_dane_archiwalne(stacja, fromd, tod):
     cluster = Cluster(['127.0.0.1'], "9042")
     session = cluster.connect('json')
     cql = "SELECT json * FROM velib where station_id =" + stacja + " and timestamp >" + fromd + " and timestamp <" + tod
+    r = session.execute(cql)
+    df = pd.DataFrame()
+    for row in r:
+        df = df.append(pd.DataFrame(row))
+    return str(df.to_json(orient="records"))
+
+@app.route("/urzedy/nazwy")
+def get_nazwy():
+    cluster = Cluster(['127.0.0.1'], "9042")
+    session = cluster.connect('json')
+    cql = "SELECT json * from urzedy_nazwy"
+    r = session.execute(cql)
+    df = pd.DataFrame()
+    for row in r:
+        df = df.append(pd.DataFrame(row))
+    return str(df.to_json(orient="records"))
+
+@app.route("/urzedy/dane/<urzad>/<fromd>/<tod>", methods=['GET'])
+def get_urzedy_dane_archiwalne(urzad, fromd, tod):
+    cluster = Cluster(['127.0.0.1'], "9042")
+    session = cluster.connect('json')
+    cql = "SELECT json * FROM urzedy where idgrupy =" + urzad + " and timestamp >" + fromd + " and timestamp <" + tod
     r = session.execute(cql)
     df = pd.DataFrame()
     for row in r:
