355ce647b2509dd96d8fd906581533e3c021cb6d
michalSt98 <32554595+michalSt98@users.noreply.github.com>
2020-11-21 19:25:36 +0100

    time_frame parameter

--- Data_for_ML/load_table.py
+++ Data_for_ML/load_table.py
@@ -1,9 +1,11 @@
 import os
 from pyspark.sql import SparkSession
+import calendar
+import time
 # W tym pliku znajdują się funkcje odpowiedzialne za tworzenie połączenia oraz wczytywanie tabel z bazy danych Cassandra
 
 
-def load_and_get_table_df(keys_space_name, table_name):
+def load_and_get_table_df(keys_space_name, table_name, time_frame=None):
     # WYmagane pakiety oraz intefejsy
     os.environ[
         'PYSPARK_SUBMIT_ARGS'] = '--packages com.datastax.spark:spark-cassandra-connector_2.12:3.0.0' \
@@ -14,4 +16,9 @@ def load_and_get_table_df(keys_space_name, table_name):
         .format("org.apache.spark.sql.cassandra")\
         .options(table=table_name, keyspace=keys_space_name)\
         .load()
+
+    if time_frame is None:
+        time_frame = calendar.timegm(time.gmtime()) - 1209600
+    table_df = table_df.filter(table_df("timestamp") > time_frame)
+
     return table_df, spark
