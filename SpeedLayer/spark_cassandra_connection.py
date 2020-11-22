8c04e02a65398f9178b0a05a94a2eae7040e6fee
michalSt98 <32554595+michalSt98@users.noreply.github.com>
2020-11-22 14:39:52 +0100

    universal path

--- SpeedLayer/spark_cassandra_connection.py
+++ SpeedLayer/spark_cassandra_connection.py
@@ -1,11 +1,16 @@
+import os
+import findspark
 # W tym pliku znajdują się funkcje odpowiedzialne za tworzenie i obsługę połączeń na lini spark-cassadnra
 
 def writeToCassandra(stream):
-    # Tworzenia połączenia spark-cassandra oraz zapisywanie strumienia danych do bazy danych Casandra
+    # Tworzenia połączenia spark-cassandra oraz zapisywanie strumienia danych do bazy danych Cassandra
+
+    checkpointLocation = os.path.join(findspark.find(), "checkpoint")
+
     query = stream.writeStream \
         .format("org.apache.spark.sql.cassandra") \
         .outputMode('append') \
-        .options(table="predictions", keyspace="predictions", checkpointLocation=r'D:\checkpoint') \
+        .options(table="predictions", keyspace="predictions", checkpointLocation=checkpointLocation) \
         .start() \
         .awaitTermination()
 
--- Tests/test.py
+++ Tests/test.py
@@ -4,6 +4,10 @@ import Data_for_ML.powietrze_manipulation
 import Data_for_ML.urzedy_manipulation
 import Data_for_ML.velib_manipulation
 import SpeedLayer.speed_connection
+import os
+import findspark
+
+path = findspark.find()
 
 # Miejse do testowania działania modułów
 
@@ -13,26 +17,29 @@ import SpeedLayer.speed_connection
 data_pow, sc_pow = Data_for_ML.powietrze_manipulation.load_powietrze()
 
 # Wytrenowanie modelu klsyfikacyjnego na wcześniej załadowanych danych
-model_pow = spark_ml.classificator.Classification.make_class_model(data_pow, sc_pow, r'D:\powietrze_model', 'RF_pow',
+powietrze_path = os.path.join(path, 'powietrze_model')
+model_pow = spark_ml.classificator.Classification.make_class_model(data_pow, sc_pow, powietrze_path, 'RF_pow',
                                                                    'pm25')
 
 # Uruchomienia modułu szybkiego przetwarzania dla powietrza, który korzysta z wcześniej nauczonych modeli
-spark_pow, query_pow, ssc_pow = SpeedLayer.speed_connection.activate_powietrze_stream()
+spark_pow, query_pow, ssc_pow = SpeedLayer.speed_connection.activate_powietrze_stream(model_path=powietrze_path)
 
 # Urzedy
 
 data_urz, sc_urz = Data_for_ML.urzedy_manipulation.load_urzedy()
 
-model_urz = spark_ml.reggresor.Regression.make_regr_model(data_urz, sc_urz, r'D:\urzedy_model', 'RF_urz',
+urzedy_path = os.path.join(path, 'urzedy_model')
+model_urz = spark_ml.reggresor.Regression.make_regr_model(data_urz, sc_urz, urzedy_path, 'RF_urz',
                                                           "liczbaKlwKolejce")
 
-spark_urz, query_urz, ssc_urz = SpeedLayer.speed_connection.activate_urzedy_stream()
+spark_urz, query_urz, ssc_urz = SpeedLayer.speed_connection.activate_urzedy_stream(model_path=urzedy_path)
 
 # Velib
 
 data_vel, sc_vel = Data_for_ML.velib_manipulation.load_velib()
 
-model_vel = spark_ml.reggresor.Regression.make_regr_model(data_vel, sc_vel, r'D:\velib_model', 'RF_vel',
+velib_path = os.path.join(path, 'velib_model')
+model_vel = spark_ml.reggresor.Regression.make_regr_model(data_vel, sc_vel, velib_path, 'RF_vel',
                                                           'numbikesavailable')
 
-spark_vel, query_vel, ssc_vel = SpeedLayer.speed_connection.activate_velib_stream()
+spark_vel, query_vel, ssc_vel = SpeedLayer.speed_connection.activate_velib_stream(model_path=velib_path)
