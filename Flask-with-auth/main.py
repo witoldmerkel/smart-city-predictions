d390085522d56ddb348387eb24ebbc9430bba19f
Witold <w.merkel@student.mini.pw.edu.pl>
2020-11-23 18:34:19 +0100

    potwierdzenie adresu email, poprzez wysłany link na maila

--- Flask-with-auth/main.py
+++ Flask-with-auth/main.py
@@ -1,7 +1,10 @@
 # Poniżej ładowane są biblioteki potrzebne do poprawnej pracy interfejsu
+
+from flask_mail import Mail, Message
 from flask import Flask
 from flask_login import UserMixin
 from flask import render_template, redirect, url_for, request, flash
+from itsdangerous import URLSafeTimedSerializer, SignatureExpired, BadSignature
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required
 from cassandra.cluster import Cluster
@@ -16,18 +19,69 @@ db = SQLAlchemy()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'thisisimportantsomehow'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'emailakceptacyjny@gmail.com'
+app.config['MAIL_PASSWORD'] = '123456780_ok'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 db.init_app(app)
 login_manager = LoginManager()
 login_manager.login_view = 'login'
 login_manager.init_app(app)
+mail = Mail(app)
+s = URLSafeTimedSerializer('akceptacjamailem')
+
+def send_mail_confirmation(user):
+    token = user.get_mail_confirm_token()
+    msg = Message(
+        "Zaakceptuj maila",
+        sender="noreply@demo.com",
+        recipients=[user.email])
+    link = url_for('confirm_email', token=token, _external=True)
+    msg.body = 'Your link is {}'.format(link)
+    mail.send(msg)
+
+@app.route('/confirm_email/<token>')
+def confirm_email(token):
+    email = User.verify_mail_confirm_token(token)
+    if email:
+        user = db.session.query(User).filter(User.email == email).one_or_none()
+        user.email_confirmed = True
+        db.session.add(user)
+        db.session.commit()
+        flash(
+            "Gratualcje, udało ci się potwierdzić mail!",
+            "Zapraszamy",
+        )
+        return redirect(url_for("login"))
+    else:
+        return render_template(url_for('singup'))
 
 
 # Model tabeli znajdującej się w bazie danych, przechowuje zarejstrowanych użytkowników
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
+    email_confirmed = db.Column(db.Boolean(), nullable=False, default=False)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+
+    def get_mail_confirm_token(self):
+        s = URLSafeTimedSerializer(
+            app.config["SECRET_KEY"], salt="email-comfirm")
+        return s.dumps(self.email, salt="email-confirm")
+
+    @staticmethod
+    def verify_mail_confirm_token(token):
+        try:
+            s = URLSafeTimedSerializer(
+                app.config["SECRET_KEY"], salt="email-confirm"
+            )
+            email = s.loads(token, salt="email-confirm", max_age=3600)
+            return email
+        except (SignatureExpired, BadSignature):
+            return None
 
 
 # Przy pierwszym uruchomieniu tworzona jest baza i tabela
@@ -65,9 +119,11 @@ def login_post():
     user = User.query.filter_by(email=email).first()
     # Sprawdzenie czy taki użytkownik ma to hasło
     if not user or not check_password_hash(user.password, password):
-        flash('Please check your login details and try again.')
+        flash('Sprawdz wprowadzone dane.')
         return redirect(url_for('login'))
-
+    if not user.email_confirmed:
+        flash('Potwierdz konto.')
+        return redirect(url_for('login'))
     login_user(user, remember=remember)
 
     return redirect(url_for('home'))
@@ -90,20 +146,21 @@ def signup_post():
     user = User.query.filter_by(email=email).first()
     # Jeżeli istnieje to przesyłamy jeszcze raz na rejstracje
     if user:
-        flash('Email address already exists.')
+        flash('Mail zajęty.')
         return redirect(url_for('signup'))
     # Tworzenie nowego wpisu do bazy danych
     new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
     # Zapisanie wpisu do bazy danych
     db.session.add(new_user)
     db.session.commit()
+    send_mail_confirmation(new_user)
 
     return redirect(url_for('login'))
 
 
 # Przekierowuje na forumalrz po wylogowaniu się
 @app.route('/logout')
-@login_required # To oznacza, że trzeba być zalogowanym, aby móv wykonać tę akcję, poniżej wszystkie endpointy tak mają
+@login_required  # To oznacza, że trzeba być zalogowanym, aby móv wykonać tę akcję, poniżej wszystkie endpointy tak mają
 # Zatem nie można się do nich dostać bez wcześniejszego zalogowania
 def logout():
     logout_user()
@@ -220,7 +277,7 @@ def get_rowery_dane_archiwalne(stacja, fromd, tod):
     return str(df.to_json(orient="records"))
 
 
-#Pobranie z bazy danych Cassandra nazw urzędów
+# Pobranie z bazy danych Cassandra nazw urzędów
 @app.route("/urzedy/nazwy")
 @login_required
 def get_nazwy():
