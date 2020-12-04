9aab7fdd923004aa9147068e29467d3eb4641f16
michalSt98 <32554595+michalSt98@users.noreply.github.com>
2020-12-04 13:20:51 +0100

    All tests for automatic model creation

--- spark_ml/tests/conftest.py
+++ spark_ml/tests/conftest.py
@@ -3,26 +3,37 @@ import pandas as pd
 import pytest
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture()
 def spark_df_class():
     spark = SparkSession.builder.getOrCreate()
     df = spark.createDataFrame([
         ("yes", 1.0, 0.0, 1.1, 0.1, "sun"),
         ("no", 0.0, 2.0, 1.0, -1.0, "moon"),
-        ("no", 0.0, 2.0, 1.3, 1.0, "left"),
+        ("picture", 0.6, 13.0, 1.0, -1.0, "fast"),
+        ("yes", 0.0, 2.0, 1.0, -1.0, "easy"),
+        ("nice", 0.2, 2.0, 2.0, -1.0, "good"),
+        ("no", 0.0, 2.0, 1.0, -1.0, "off"),
+        ("ok", 0.5, 2.0, 1.0, -1.0, "on"),
+        ("no", 0.0, 2.0, 1.3, 4.0, "left"),
         ("yes", 1.0, 0.0, 1.2, -0.5, "right")], ["target", "v1", "v2", "v3", "v4", "v5"])
     yield spark, df
     spark.stop()
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture()
 def spark_df_regr():
     spark = SparkSession.builder.getOrCreate()
+    spark = spark.newSession()
     df = spark.createDataFrame([
         ("yes", 1.0, 0.0, 1.1, 0.1, "sun"),
-        ("no", 0.0, 2.0, 1.0, -1.0, "moon"),
+        ("but", 0.0, 2.0, 1.0, -1.0, "moon"),
+        ("picture", 0.6, 13.0, 1.0, -1.0, "fast"),
+        ("yes", 0.0, 2.0, 1.0, -1.0, "easy"),
+        ("nice", 0.2, 2.0, 2.0, -1.0, "good"),
+        ("no", 0.0, 2.0, 1.0, -1.0, "off"),
+        ("ok", 0.5, 2.0, 1.0, -1.0, "on"),
         ("no", 0.0, 2.0, 1.3, 1.0, "left"),
-        ("yes", 1.0, 0.0, 1.2, -0.5, "right")], ["v1", "target", "v2", "v3", "v4", "v5"])
+        ("or", 1.0, 0.0, 1.2, -0.5, "right")], ["v1", "target", "v2", "v3", "v4", "v5"])
     yield spark, df
     spark.stop()
 
--- spark_ml/tests/test_regression.py
+++ spark_ml/tests/test_regression.py
@@ -1,18 +1,19 @@
-from Classification import make_class_model
+from Regression import make_regr_model
 import findspark
 import os
 from time import time
 from cassandra.cluster import Cluster
 import pytest
 
+
 # Test sprawdzający czy powstały automatycznie model ma wszystkie wymagane etapy
-def test_make_class_model_pipeline(spark_df_class):
-    spark, df = spark_df_class
+def test_make_regr_model_pipeline(spark_df_regr):
+    spark, df = spark_df_regr
     path = findspark.find()
     path = os.path.join(path, 'models')
-    test_path = os.path.join(path, 'test_class_model')
+    test_path = os.path.join(path, 'test_regr_model')
     test_path = test_path + '_' + str(int(time()))
-    model, _ = make_class_model(df, spark, test_path, "test_class_model", "test_target",
+    model, _ = make_regr_model(df, spark, test_path, "test_regr_model", "test_target",
                                                               save=False)
     actual_stages = str([type(x) for x in model.stages])
     expected_stages = "[<class 'pyspark.ml.feature.StringIndexerModel'>," \
@@ -22,26 +23,25 @@ def test_make_class_model_pipeline(spark_df_class):
                       " <class 'pyspark.ml.feature.VectorAssembler'>," \
                       " <class 'pyspark.ml.feature.StandardScalerModel'>," \
                       " <class 'pyspark.ml.feature.VectorAssembler'>," \
-                      " <class 'pyspark.ml.classification.RandomForestClassificationModel'>," \
-                      " <class 'pyspark.ml.feature.IndexToString'>]"
+                      " <class 'pyspark.ml.regression.RandomForestRegressionModel'>]"
     assert actual_stages == expected_stages
 
 
 # Test sprawdzający czy model poprawnie zapisuje się do tabeli w bazie danych Cassandra
-def test_make_class_model_saving(spark_df_class, pandas_factory_fixture):
-    spark, df = spark_df_class
+def test_make_regr_model_saving(spark_df_regr, pandas_factory_fixture):
+    spark, df = spark_df_regr
     path = findspark.find()
     path = os.path.join(path, 'models')
-    test_path = os.path.join(path, 'test_class_model')
+    test_path = os.path.join(path, 'test_regr_model')
     test_path = test_path + '_' + str(int(time()))
-    make_class_model(df, spark, test_path, "test_class_model", "test_target",
+    make_regr_model(df, spark, test_path, "test_regr_model", "test_target",
                                 save=True)
     cluster = Cluster(['127.0.0.1'], "9042")
     session = cluster.connect("models")
     session.row_factory = pandas_factory_fixture
     session.default_fetch_size = None
-    query_latest = "Select Max(timestamp) from models_statistics where model_name = 'test_class_model'"
-    query_path = ("Select model_path from models_statistics where timestamp = %s and model_name = 'test_class_model'"
+    query_latest = "Select Max(timestamp) from models_statistics where model_name = 'test_regr_model'"
+    query_path = ("Select model_path from models_statistics where timestamp = %s and model_name = 'test_regr_model'"
                   " ALLOW FILTERING")
     latest_timestamp = session.execute(query_latest, timeout=None)._current_rows.iloc[0]['system.max(timestamp)']
     query_path = query_path % latest_timestamp
@@ -50,4 +50,3 @@ def test_make_class_model_saving(spark_df_class, pandas_factory_fixture):
     expected_path = test_path
     actual_path = path
     assert actual_path == expected_path
-
