f63c4d18b94e6dcc2798ef0ef245c8248f96020a
michalSt98 <32554595+michalSt98@users.noreply.github.com>
2020-12-19 13:21:19 +0100

    Changed checkpoints location

--- speed_layer/spark_cassandra_connection.py
+++ speed_layer/spark_cassandra_connection.py
@@ -1,5 +1,6 @@
 import os
 import findspark
+from time import time
 # W tym pliku znajdują się funkcje odpowiedzialne za tworzenie i obsługę połączeń na lini spark-cassadnra
 
 
@@ -7,6 +8,8 @@ def writeToCassandra(stream, checkpoint="checkpoint"):
     # Tworzenia połączenia spark-cassandra oraz zapisywanie strumienia danych do bazy danych Cassandra
 
     checkpointLocation = os.path.join(findspark.find(), checkpoint)
+    checkpointLocation = os.path.join(checkpointLocation, checkpoint)
+    checkpointLocation = checkpointLocation + '_' + str(int(time()))
 
     query = stream.writeStream \
         .format("org.apache.spark.sql.cassandra") \
