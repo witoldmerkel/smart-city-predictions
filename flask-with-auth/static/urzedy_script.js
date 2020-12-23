e0a22dfd085d1208a965f2485b56e9f6a836de02
Witold <w.merkel@student.mini.pw.edu.pl>
2020-12-23 13:07:21 +0100

    predictions, time correction

--- flask-with-auth/static/urzedy_script.js
+++ flask-with-auth/static/urzedy_script.js
@@ -183,7 +183,7 @@
                     tabela_pred.appendChild(singleRow_pred);
                     for (i=0; i < response.length; i=i+15){
                         var singleRow_pred = document.createElement('tr');
-                        const dateObject = new Date((JSON.parse(response[i][0]).timestamp + 14400) * 1000)
+                        const dateObject = new Date((JSON.parse(response[i][0]).timestamp + 14400 - 3600) * 1000)
                         const humanDateFormat = dateObject.toLocaleString()
                         singleRow_pred.innerHTML += '<td>' + humanDateFormat + '</td>';
                         singleRow_pred.innerHTML += '<td>' + Math.round(JSON.parse(response[i][0]).prediction) + '</td>';
